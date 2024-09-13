e02471f6377db5e3959881a72791e31cda7f966d
MrEliptik <victormeunier.dev@gmail.com>
2024-09-13 22:46:25 +0200

    reenable pause when losing focus to prevent gamepad events from being received

--- global_shortcut/global_shortcut.gd
+++ global_shortcut/global_shortcut.gd
@@ -6,6 +6,7 @@ var ws_socket: WebSocketPeer = WebSocketPeer.new()
 var websocket_url = "ws://localhost:65432"
 
 func _init() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	set_process(false)
 	if ws_socket.connect_to_url(websocket_url) != OK:
 		print("Could not connect.")
@@ -16,22 +17,9 @@ func _init() -> void:
 	var timer: Timer = Timer.new()
 	timer.one_shot = false
 	timer.timeout.connect(process_ws)
+	timer.process_mode = Node.PROCESS_MODE_ALWAYS
 	Engine.get_main_loop().root.add_child.call_deferred(timer)
 	timer.start.call_deferred(0.5)
-
-func _ready():
-	set_process(false)
-	if ws_socket.connect_to_url(websocket_url) != OK:
-		print("Could not connect.")
-		return
-		
-	print("Connected to: ", websocket_url)
-	
-	var timer: Timer = Timer.new()
-	timer.one_shot = false
-	timer.timeout.connect(process_ws)
-	add_child(timer)
-	timer.start(0.5)
 	
 func process_ws() -> void:
 	ws_socket.poll()
